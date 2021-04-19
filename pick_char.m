5f10fd731da671672d2bdbe0fa3c7c3be6cf03cd
camillahallin <caha1111@colorado.edu>
2021-04-18 21:05:26 -0600

    I forget what I changed

--- pick_char.m
+++ pick_char.m
@@ -1,7 +1,6 @@
 function c=pick_char(char_weights)
-
+% Select characteristic point based off of weight of point
 val=rand();
-
 for i=1:length(char_weights)
     if val<sum(char_weights(1:i))
         c=i;
