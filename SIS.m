daf2eabc529a9308a9d0339c6f4090964a4a4771
jamc3951 <jamc3951@colorado.edu>
2021-04-19 20:20:45 -0600

    BS + arb. configs

--- SIS.m
+++ SIS.m
@@ -12,8 +12,9 @@ for j = 1:C_s
     outcomes(j) = o;
     current_kp(j,:) = c(2,:);
     adversary_kp(j,:) = a(2,:);
+
     
-    char_weights(j)=mvnpdf([current_kp(j,:),adversary_kp(j,:)],ykp(:)',abs([current_kp(j,:),adversary_kp(j,:)]- ykp(:)').*eye(4) + eye(4));
+    char_weights(j)=mvnpdf([current_kp(j,:),adversary_kp(j,:)],ykp,abs([current_kp(j,:),adversary_kp(j,:)]- ykp).*eye(4) + eye(4));
     
    
     if outcomes(j) == 1
@@ -27,12 +28,15 @@ char_weights=char_weights./sum(char_weights);
 %weighted_mean = mean([current_kp(:,:),adversary_kp(:,:)].*char_weights',1);
 
 for i = 1:(N_s-C_s)
+
     c = datasample(1:C_s,1,'Weights',char_weights); %sample by weights
 
     %P = ([current_kp(c,:),adversary_kp(c,:)] - weighted_mean)'*([current_kp(c,:),adversary_kp(c,:)] - weighted_mean); %-  weighted_mean*weighted_mean';
     
     current_c = mvnrnd(current_kp(c,:),10*eye(2));
     adversary_c = mvnrnd(adversary_kp(c,:),10*eye(2));
+    Ps=5;
+    Py=10;
 
 %     Sh(i)=0;
 %     for j=1:size(current_kp,1)
@@ -40,11 +44,13 @@ for i = 1:(N_s-C_s)
 %     end
 %  
     if outcomes(c) == 1 %Variance?
-        outcomes_kp(2) = outcomes_kp(2) + mvnpdf([current_c,adversary_c],ykp(:)',abs([current_c,adversary_c]- ykp(:)').*eye(4) + eye(4)).*...
-            mvnpdf([current_c,adversary_c],[current_kp(c,:),adversary_kp(c,:)],(abs([current_kp(c,:),adversary_kp(c,:)]- ykp(:)')).*eye(4) + eye(4));
+
+        outcomes_kp(2) = outcomes_kp(2) + mvnpdf([current_c,adversary_c],ykp,abs([current_c,adversary_c]- ykp).*eye(4) + eye(4)).*...
+            mvnpdf([current_c,adversary_c],[current_kp(c,:),adversary_kp(c,:)],Ps*eye(4));
     else
-        outcomes_kp(1) = outcomes_kp(1) +  mvnpdf([current_c,adversary_c],ykp(:)',abs([current_c,adversary_c]- ykp(:)').*eye(4) + eye(4)).*...
-            mvnpdf([current_c,adversary_c],[current_kp(c,:),adversary_kp(c,:)],(abs([current_kp(c,:),adversary_kp(c,:)]- ykp(:)')).*eye(4) + eye(4));
+        outcomes_kp(1) = outcomes_kp(1) +  mvnpdf([current_c,adversary_c],ykp,abs([current_c,adversary_c]- ykp).*eye(4) + eye(4)).*...
+            mvnpdf([current_c,adversary_c],[current_kp(c,:),adversary_kp(c,:)],Ps*eye(4));
+
     end
 end
 t = sum(outcomes_kp);
